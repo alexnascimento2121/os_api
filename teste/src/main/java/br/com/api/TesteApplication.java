f082ed7fe2483d7e41bb7aa08d0c52ee66508bda
alex.goncalves <alexnascimento2121@outlook.com>
2020-10-29 15:42:47 -0400

    initial commit

---
+++ teste/src/main/java/br/com/api/TesteApplication.java
@@ -0,0 +1,13 @@
+package br.com.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteApplication.class, args);
+	}
+
+}
---
+++ teste/src/main/java/br/com/api/repository/ClienteRepository.java
@@ -0,0 +1,17 @@
+package br.com.api.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.api.model.Cliente;
+//anotação do spring, que define como repositorio similar ao DAO do JSF. através dele eu posso herdar inumeras dependencias.
+@Repository 
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+	List<Cliente> findByNome(String nome);
+	
+	//busca nome com letras e expressoes similares. igual ao like%nome%
+	List<Cliente>  findByNomeContaining	(String nome);
+	
+}
